a75eeedc0a5b3062641a3c1ae1847c1234ee895a
shynome <shynome@gmail.com>
2017-04-05 20:46:14 +0800

    添加模板热更新选项

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,23 @@ import *as React from 'react';
 declare global {
   var React:any
 }
-import { renderToString } from 'react-dom/server';
-
-export let render = (filepath:string,options:any,cb)=>{
+import *as ReactDOM from 'react-dom/server';
+export type option = {
+  /**模板热更新 */
+  hotload?:boolean
+  /**默认不保留 react 标记 */
+  renderMethod?:'renderToString'|'renderToStaticMarkup'
+}
+export let render = ({ renderMethod='renderToStaticMarkup', hotload=false, }:option = {})=>
+(filepath:string,options:any,cb)=>{
+      filepath = require.resolve(filepath)
+  if( hotload ){
+    delete require.cache[filepath]
+  }
   let exports = require(filepath)
   let Render = exports && exports.default || exports
   let jsx = React.createElement(Render,options)
-  let html = renderToString(jsx)
+  let html = ReactDOM[renderMethod](jsx)
   cb(null,html)
 }
 export let __express = render
