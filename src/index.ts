ff04483f5e64b42cd86c3d62b90464137f93e826
shynome <shynome@gmail.com>
2017-04-16 12:46:26 +0800

    更改 options 配置方式

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,28 @@ declare global {
   var React:any
 }
 import *as ReactDOM from 'react-dom/server';
-export type option = {
+export class Option {
   /**模板热更新 */
-  hotload?:boolean
+  hotload?:boolean = false
   /**服务器渲染函数 */
-  renderMethod?:'renderToString'|'renderToStaticMarkup'
+  renderMethod?:'renderToString'|'renderToStaticMarkup' = 'renderToString'
 }
 /**默认配置 */
-export let option:option = {
-  hotload : false,
-  renderMethod : 'renderToStaticMarkup'
-}
-export let render = ({ renderMethod=option.renderMethod, hotload=option.hotload, }:option = {})=>
-(filepath:string,options:any,cb)=>{
-      filepath = require.resolve(filepath)
-  if( hotload ){
-    delete require.cache[filepath]
+export let defaultOption:Option = new Option()
+import *as configExtend from "config-extend";
+export let render = (option:Option=defaultOption)=>{
+  const { renderMethod, hotload }:Option = configExtend({},defaultOption,option)
+  return (filepath:string,options:any,cb)=>{
+        filepath = require.resolve(filepath)
+    if( hotload ){
+      delete require.cache[filepath] //粗暴地实现了热载
+    }
+    let exports = require(filepath)
+    let Render = exports && exports.default || exports
+    let jsx = React.createElement(Render,options)
+    let html = ReactDOM[renderMethod](jsx)
+    cb(null,html)
   }
-  let exports = require(filepath)
-  let Render = exports && exports.default || exports
-  let jsx = React.createElement(Render,options)
-  let html = ReactDOM[renderMethod](jsx)
-  cb(null,html)
 }
 export let __express = render
 export default render
