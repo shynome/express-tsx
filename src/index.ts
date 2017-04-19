40d2a2ec49420cbd253b0ed018396f45dfafff6b
shynome <shynome@gmail.com>
2017-04-19 22:01:58 +0800

    优化命名

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import *as React from 'react';
+import React = require('react');
 (global as any).React = React
 declare global {
   var React:any
 }
-import *as ReactDOM from 'react-dom/server';
-export class Option {
+import ReactDOM = require('react-dom/server');
+export class Options {
   /**模板热更新 */
   hotload?:boolean = false
   /**服务器渲染函数 */
@@ -13,20 +13,19 @@ export class Option {
   doctype?:string = '<!DOCTYPE html>'
 }
 /**默认配置 */
-export let defaultOption:Option = new Option()
-import *as configExtend from "config-extend";
-export let render = (option:Option=defaultOption)=>{
-  const { renderMethod, hotload,doctype }:Option = configExtend({},defaultOption,option)
-  return (filepath:string,options:any,cb)=>{
+export let defaultOptions:Options = new Options()
+import configExtend = require("config-extend")
+export function render(options:Options=defaultOptions){
+  const { renderMethod, hotload, doctype }:Options = configExtend({},defaultOptions,options)
+  return function(filepath:string, data:any, cb){
         filepath = require.resolve(filepath)
     if( hotload ){
       delete require.cache[filepath] //粗暴地实现了热载
     }
     let exports = require(filepath)
     let Render = exports && exports.default || exports
-    let jsx = React.createElement(Render,options)
-    let html:string = ''
-        html += doctype
+    let jsx = React.createElement(Render,data)
+    let html:string = doctype
         html += ReactDOM[renderMethod](jsx)
     cb(null,html)
   }
