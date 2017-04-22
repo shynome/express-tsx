1cbeefd795066bfbb420d565628cce7911d6b1e3
shynome <shynome@gmail.com>
2017-04-22 18:12:06 +0800

    支持服务器渲染了

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React = require('react');
+
+export const BowserRender = (appModule,data)=>
+`
+<script>
+require(['${appModule}'],function(appModule){
+  var App = appModule && appModule.default || appModule
+  ReactDOM.render(
+    React.createElement(App,${JSON.stringify(data)}),
+    document.getElementById('app')
+  )
+})
+</script>
+`
+
+export const WrapApp = (app,{ lang='zh-hms-cn', title='title', heads=[] },children=[])=>
+`
+<html lang=${lang}>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  ${heads.join('')}
+  <title>${title}</title>
+  <script src="//cdn.bootcss.com/react/15.5.4/react.js"></script>
+  <script src="//cdn.bootcss.com/react/15.5.4/react-dom.js"></script>
+  <script src="//cdn.bootcss.com/require.js/2.3.3/require.min.js"></script>
+</head>
+<body>
+  <div id="app">${app}</div>
+  ${children.join('')}
+</body>
+</html>
+`
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,68 @@
 import React = require('react');
 (global as any).React = React
-declare global {
-  var React:any
-}
+declare global { var React:any }
+import path = require('path')
 import ReactDOM = require('react-dom/server');
+import ts = require('typescript')
+
 export class Options {
   /**模板热更新 */
   hotload?:boolean = false
-  /**服务器渲染函数 */
-  renderMethod?:'renderToString'|'renderToStaticMarkup' = 'renderToString'
   /**文档类型 */
   doctype?:string = '<!DOCTYPE html>'
+  /**服务器同构渲染 */
+  ssr?:boolean = false
+  /**编译选项 */
+  compilerOptions?:ts.CompilerOptions = {
+    allowJs:true,
+    module:ts.ModuleKind.AMD,
+    target:ts.ScriptTarget.ES5,
+    jsx:ts.JsxEmit.React,
+    outFile:'bundle.js',
+    inlineSourceMap:true,
+    inlineSources:true,
+  }
+  ssrRender ?= async(Render,data,filepath:string,compile:(filepath:string)=>Promise<string>)=>{
+    let app = React.createElement(Render,data)
+    let appModuleName = path.basename(filepath,path.extname(filepath))
+    let appDefineScript = `<script>${await compile(filepath)}</script>`
+    return WrapApp(ReactDOM.renderToString(app),data,[
+      appDefineScript,
+      BowserRender(appModuleName,data),
+    ])
+  }
 }
 /**默认配置 */
 export let defaultOptions:Options = new Options()
 import configExtend = require("config-extend")
+
+export const getCompile = (compilerOptions:ts.CompilerOptions)=>(file:string):Promise<string>=>
+new Promise((resolve,reject)=>{
+  ts.createProgram([file],compilerOptions).emit(undefined,(outFile,outputText)=>{
+    resolve(outputText)
+  })
+})
+
+import { BowserRender, WrapApp } from './App'
+
 export function render(options:Options=defaultOptions){
-  const { renderMethod, hotload, doctype }:Options = configExtend({},defaultOptions,options)
-  return function(filepath:string, data:any, cb){
+  const { hotload, doctype, ssr, compilerOptions, ssrRender }:Options = configExtend({},defaultOptions,options)
+  let compile = getCompile(compilerOptions)
+  return async function(filepath:string, data:any, cb){
         filepath = require.resolve(filepath)
     if( hotload ){
       delete require.cache[filepath] //粗暴地实现了热载
     }
     let exports = require(filepath)
     let Render = exports && exports.default || exports
-    let jsx = React.createElement(Render,data)
     let html:string = doctype
-        html += ReactDOM[renderMethod](jsx)
+    if(ssr){
+      html += await ssrRender(Render,data,filepath,compile)
+    }else{
+      html += ReactDOM.renderToStaticMarkup(
+        React.createElement(Render,data)
+      )
+    }
     cb(null,html)
   }
 }
