d26da7fb24c04a6c9ea6cb27086bc3c47f11b5b9
shynome <shynome@gmail.com>
2017-05-21 05:22:50 +0800

    类型更新下

--- example/http/http2.ts
+++ example/http/http2.ts
@@ -2,13 +2,13 @@ import http2 = require('spdy')
 
 import { readFileSync } from "fs";
 import { join } from "path";
-const agentOptions:http2.AgentOptions = {
+const agentOptions:http2.ServerOptions = {
   key:readFileSync(join(__dirname,'./certificate/localhost.key')),
   cert:readFileSync(join(__dirname,'./certificate/localhost_bundle.crt')),
 }
 
 import { app } from "./app";
-export const server = http2.createServer(agentOptions,app)
+export const server = http2.createServer(agentOptions,app as any)
 
 import { https_PORT,log } from "../";
 server.listen(https_PORT,()=>{
