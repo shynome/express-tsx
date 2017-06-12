1528f522317adec356e9d71e78bd677f5ec8dda4
shynome <shynome@gmail.com>
2017-06-13 02:19:01 +0800

    css in js 案例更新

--- example/views/ssrRender.tsx
+++ example/views/ssrRender.tsx
@@ -1,8 +1,16 @@
 import React = require('react')
 import *as deep2 from './deep1'
+import glamorous from 'glamorous'
+const MyStyle = glamorous.div({
+  color:'red',
+  transition:'all 3s',
+  ':hover':{
+    color:'yellow'
+  }
+})
 export default (props)=>
-<div onClick={ ()=>alert(props.who) }>
+<MyStyle onClick={ ()=>alert(props.who) }>
   { JSON.stringify(deep2) }
   <br/>
   { JSON.stringify(props) }
-</div>
+</MyStyle>
