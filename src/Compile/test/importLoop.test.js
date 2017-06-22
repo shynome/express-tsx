e7ed21c7b496f53de9adf67eb4c35adb3e110aa7
shynome <shynome@gmail.com>
2017-06-23 00:21:40 +0800

    getImport 使用时可不用提前申明该文件在 需要编译的文件中了

--- src/Compile/test/importLoop.test.js
+++ src/Compile/test/importLoop.test.js
@@ -1,6 +1,6 @@
 require('ts-node').register({ fast:true, project:process.cwd() })
 const path = require('path')
 const { compile } = require('../')
-const entryFile = compile.scriptVersion[path.join(__dirname,'./b.test.ts')].filename
-let deps = compile.getAllImports(entryFile)
+const entryFile = path.join(__dirname,'./b.test.ts')//compile.scriptVersion[path.join(__dirname,'./b.test.ts')].filename
+let deps = compile.getImports(entryFile)
 console.log(deps)
