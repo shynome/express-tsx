6b417af997e984f2505677e512f8852926222a8c
shynome <shynome@gmail.com>
2017-07-13 22:13:12 +0800

    实现了 jsonp 接口

---
+++ src/render.ts
@@ -0,0 +1,48 @@
+
+import { Router,Response } from "express";
+export const renderMiddleware = Router()
+renderMiddleware.use((req,res,next)=>{
+  res.locals.callback = req.query.callback
+  res.locals.res = res
+  next()
+})
+export class ServerData {
+  //express-tsx addon
+  callback:string|undefined = null
+  res:Response = null
+  //express server data
+  _locals:object = null
+  cache:boolean = null
+  settings:object = null
+  static need_delete_fields = Object.keys(new ServerData())
+  static filter = (data:data):ViewData=>{
+    let filtered_data:any = {}
+    for(let key in data){
+      if(ServerData.need_delete_fields.includes(key)){ continue }
+      filtered_data[key] = data[key]
+    }
+    return filtered_data
+  }
+}
+export type ViewData = {
+  title:string
+  lang:string
+  [key:string]:any
+}
+export type data = ServerData & ViewData
+
+export type ViewEngine = (file:string,data:data,next:(error:Error|null,rendered?:string)=>void)=>void
+import { html } from './html'
+export const render:ViewEngine = async(file,data,next)=>(async(file,data):Promise<string>=>{
+  let callback = data.callback
+  let view_data = ServerData.filter(data)
+  if(typeof callback === 'string'){
+    let json = JSON.stringify(view_data)
+    if( !callback.length ){ return json }
+    return `${callback}(${json})`
+  }
+  return html(file,data)
+})(file,data).then(
+  (res)=>next(null,res),
+  (err)=>next(err),
+)
