cda49f5581c155f52a9afd88cce77339284e49c5
shynome <shynome@gmail.com>
2017-07-16 04:44:19 +0800

    fail : can't remove those imports file which  the only type be used

---
+++ test/views/Article.ts
@@ -0,0 +1,4 @@
+export interface Article {
+  title:string
+  content:string 
+}
--- test/views/index.tsx
+++ test/views/index.tsx
@@ -1,5 +1,8 @@
 import React = require('react')
 import *as obj from './end'
+import { Article } from './Article';
+let a:Article = null
+console.log(a)
 export default ()=>
 <div>
   { JSON.stringify(obj) }
