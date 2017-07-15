66c68fe6562d96e663848626435693af688983e5
shynome <shynome@gmail.com>
2017-07-16 07:09:11 +0800

    fixed mistake

--- example/server.js
+++ example/server.js
@@ -1,7 +1,7 @@
 //创建 express 服务
 const express = require('express')
 const server = express()
-server.listen(9000,function(){ console.log(`server is running on ${this.addresss().port}`) })
+server.listen(9000,function(){ console.log(`server is running on ${this.address().port}`) })
 //设定视图引擎
 server.engine('.tsx',require('express-tsx').render)
 server.set('views',__dirname)
@@ -9,4 +9,4 @@ server.set('view engie','tsx')//选择视图引擎
 //**注意**:在渲染视图前需要根路由注入中间件
 server.use(require('express-tsx').middleware)
 //渲染视图
-server.ues('/',(req,res)=>res.render('./view.tsx'))
+server.use('/',(req,res)=>res.render('./view.tsx'))
