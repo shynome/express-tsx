2fa12afe12ef9c96e6807f0737454a09a545ebbf
shynome <shynome@gmail.com>
2017-07-16 20:26:29 +0800

    remove gulp for build , just npm scripts is enough

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const exec = (cammand)=>new Promise((rl,rj)=>require('child_process').exec(cammand,rl))
-gulp.task(
-  'build',
-  ()=>Promise.resolve(1)
-  .then(()=>exec('rm -r lib'))
-  .then(()=>exec('tsc -p src'))  
-)
