798bc55b4bac86c6aabbf44ac2a35af1b00887aa
shynome <shynome@gmail.com>
2017-07-24 00:26:36 +0800

    fix

--- src/render/requirejs.config.ts
+++ src/render/requirejs.config.ts
@@ -7,7 +7,7 @@ requirejs.config({
     'requirejs'       :'//cdn.bootcss.com/require.js/2.3.3/require.min.js#',
     'css'             :'//cdn.bootcss.com/require-css/0.1.10/css.min.js#',
     'es5-shim'        :'//cdn.bootcss.com/es5-shim/4.5.9/es5-shim.min.js#',
-    'es6-shim'        :'//cdn.bootcss.com/es6-shim/0.35.3/es6-shim.min.js',
+    'es6-shim'        :'//cdn.bootcss.com/es6-shim/0.35.3/es6-shim.min.js#',
   },
   shim:{
     'react'           :{ deps:['es6-shim','es5-shim'] },
