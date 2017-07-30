468e6c97b5f964660e65604746b57b6dde11cb53
shynome <shynome@gmail.com>
2017-07-30 12:19:11 +0800

    update cdn server

--- src/render/requirejs.config.ts
+++ src/render/requirejs.config.ts
@@ -1,16 +1,16 @@
 import path = require('path')
 import requirejs = require('requirejs')
 requirejs.config({
+  baseUrl:'https://unpkg.com/',
   paths:{
-    'react'           :'//cdn.bootcss.com/react/15.6.1/react.js#',
-    'react-dom'       :'//cdn.bootcss.com/react/15.6.1/react-dom.js#',
-    'requirejs'       :'//cdn.bootcss.com/require.js/2.3.3/require.min.js#',
-    'css'             :'//cdn.bootcss.com/require-css/0.1.10/css.min.js#',
-    'es5-shim'        :'//cdn.bootcss.com/es5-shim/4.5.9/es5-shim.min.js#',
-    'es6-shim'        :'//cdn.bootcss.com/es6-shim/0.35.3/es6-shim.min.js#',
+    'requirejs'       :'requirejs/require.js#',
+    'react'           :'react/dist/react.js#',
+    'react-dom'       :'react-dom/dist/react-dom.js#',
+    'css'             :'require-css/css.min.js#',
+    'es6-shim'        :'es6-shim/es6-shim.min.js#',
   },
   shim:{
-    'react'           :{ deps:['es6-shim','es5-shim'] },
+    'react'           :{ deps:['es6-shim'] },
   }
 })
 export const getRequirejsConfig = ()=>requirejs.s.contexts._.config
