c7b3b34fddcdd81c5a3b35e26da9181542fa5f5e
shynome <shynome@gmail.com>
2017-08-22 23:26:26 +0800

    更多文件的 hotreload 测试

--- example/views/index.tsx
+++ example/views/index.tsx
@@ -1,16 +1,20 @@
 import React = require('react')
 import { App } from './App';
-export const props = require('?state');import '?state'
-//init
-props.value = props.value || 1
-console.log(props)
-export class View extends React.Component<any,any>{
+import *as u from './header'
+// sdfdasf
+class State {
+  title = 'name'
+}
+export const props = new State()
+export class View extends React.Component<State,void>{
   render(){
     console.log(this.props)
     return (
-    <form action="#" onSubmit={(e)=>{e.preventDefault();props.value+=1;console.log(props)}}>
-      <input type="text" defaultValue={this.props.value} />
-    </form>
+    <App>
+      <form action="#" onSubmit={(e)=>{e.preventDefault();}}>
+        <input type="text" defaultValue={'5555'} />
+      </form>
+    </App>
     )
   }
 }
