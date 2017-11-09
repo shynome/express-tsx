ea10b16feb0377a2b1076d99e6725f7d77127efc
shynome <shynome@gmail.com>
2017-11-09 21:49:40 +0800

    热更新可以了但是会重复加载文件

--- browser/init.ts
+++ browser/init.ts
@@ -5,10 +5,9 @@ declare var EventSource:any
 define('?props',[location.href.split('#')[0]+(location.href.indexOf('?')===-1?'?':'')+'&callback=define'],(data)=>data) 
 void function(){//scope
   
-const isNavtiveFunc = /\[navtive code\]/i
+const isNavtiveFunc = /\[native code\]/i
 const noop = ()=>{}
 isNavtiveFunc.test(Promise.toString()) && define('es6-shim',[],noop)
-debugger
 
 const currentScript = document.scripts[document.scripts.length-1]
 let imports:string[] = JSON.parse(currentScript.innerText) 
@@ -38,8 +37,8 @@ class Manager {
     let short_name = name.replace(Manager.regx.index,'') 
     let modules = [ name, module, short_name, ] 
     if(name === this.main){ this.entry = module } 
-    modules.forEach(m=>requirejs.undef(m)) 
-    return Manager.defineModule(module) 
+    modules.forEach(m=>requirejs.undef(m))
+    return Manager.defineModule(module)
   }
   constructor(imports:string[]){
     this.entry = imports[0] 
@@ -60,21 +59,35 @@ function(react,ReactDOM,exports){
       this.state = this.props
       if(dev){
         let watcher = new EventSource(hotreload) 
-        watcher.addEventListener('hotreload',this.update)
+        watcher.addEventListener('hotreload',(e)=>{
+          const tpl = (w:string)=>`[ ${w} ]`
+          console.log(tpl('receive update event'))
+          this.update(e,()=>console.log(tpl('update finished')))
+        })
       }
     }
-    update = ({ data })=>{
-      let module:string[] = JSON.parse(data) 
-      module.forEach(manager.updateModule)
-      requirejs([manager.main],(exports)=>{
-        this.setState({
-          view: getView(exports),
-          props: exports.props,
-        })
-      })
+    update = ({ data }, cb:(err?:any)=>void=noop)=>{
+      let module:string[] = JSON.parse(data);
+      [manager.entry].concat(module).forEach(manager.updateModule)
+      this.setState({ error: null })
+      requirejs(
+        [manager.main],
+        (exports)=>{
+          this.setState({ view: getView(exports), props: exports.props, error:null })
+          cb(null)
+        },
+        (err)=>{
+          err.requireModules.forEach(m=>requirejs.undef(m))
+          cb(err)
+          throw err
+        }
+      )
     }
     render(){
-      let { view, props } = this.state
+      let { view, props, error } = this.state
+      if(this.error){
+        return react.createElement('pre',{},this.error)
+      }
       return react.isValidElement(view)?view:react.createElement(view,props)
     }
   }
